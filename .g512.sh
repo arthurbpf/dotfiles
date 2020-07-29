38af078be50076447c93dc574f93af3ece8ba0f7
Arthur Fernandes <arthurbpfernandes@gmail.com>
2020-07-28 21:11:28 -0300

    Gradient

--- .g512.sh
+++ .g512.sh
@@ -5,4 +5,105 @@
 #Product ID: c342
 
 #Startup script to define colors
-g512-led -a ddded9
+#g512-led -a ddded9
+
+g512-led -kn esc ffffff
+g512-led -gn fkeys ffffff
+g512-led -kn printscreen ffffff
+g512-led -kn scrolllock ffffff
+g512-led -kn pause_break ffffff
+
+g512-led -kn tilde c3c3c3
+g512-led -kn 1 c3c3c3
+g512-led -kn 2 c3c3c3
+g512-led -kn 3 c3c3c3
+g512-led -kn 4 c3c3c3
+g512-led -kn 5 c3c3c3
+g512-led -kn 6 c3c3c3
+g512-led -kn 7 c3c3c3
+g512-led -kn 8 c3c3c3
+g512-led -kn 9 c3c3c3
+g512-led -kn 0 c3c3c3
+g512-led -kn minus c3c3c3
+g512-led -kn equal c3c3c3
+g512-led -kn backspace c3c3c3
+g512-led -kn insert c3c3c3
+g512-led -kn home c3c3c3
+g512-led -kn page_up c3c3c3
+g512-led -kn num_lock c3c3c3
+g512-led -kn num_slash c3c3c3
+g512-led -kn num_asterisk c3c3c3
+g512-led -kn num_minus c3c3c3
+
+g512-led -kn tab b1b1b1
+g512-led -kn q b1b1b1
+g512-led -kn w b1b1b1
+g512-led -kn e b1b1b1
+g512-led -kn r b1b1b1
+g512-led -kn t b1b1b1
+g512-led -kn y b1b1b1
+g512-led -kn u b1b1b1
+g512-led -kn i b1b1b1
+g512-led -kn o b1b1b1
+g512-led -kn p b1b1b1
+g512-led -kn open_bracket b1b1b1
+g512-led -kn close_bracket b1b1b1
+g512-led -kn backslash b1b1b1
+g512-led -kn delete b1b1b1
+g512-led -kn end b1b1b1
+g512-led -kn pagedown b1b1b1
+g512-led -kn num7 b1b1b1
+g512-led -kn num8 b1b1b1
+g512-led -kn num9 b1b1b1
+g512-led -kn numplus b1b1b1
+
+g512-led -kn caps_lock c2c2c2
+g512-led -kn a c2c2c2
+g512-led -kn s c2c2c2
+g512-led -kn d c2c2c2
+g512-led -kn f c2c2c2
+g512-led -kn g c2c2c2
+g512-led -kn h c2c2c2
+g512-led -kn j c2c2c2
+g512-led -kn k c2c2c2
+g512-led -kn l c2c2c2
+g512-led -kn semicolon c2c2c2
+g512-led -kn quote c2c2c2
+g512-led -kn enter c2c2c2
+g512-led -kn num4 c2c2c2
+g512-led -kn num5 c2c2c2
+g512-led -kn num6 c2c2c2
+
+g512-led -kn shift_left 878787
+g512-led -kn z 878787
+g512-led -kn x 878787
+g512-led -kn c 878787
+g512-led -kn v 878787
+g512-led -kn b 878787
+g512-led -kn n 878787
+g512-led -kn m 878787
+g512-led -kn comma 878787
+g512-led -kn period 878787
+g512-led -kn slash 878787
+g512-led -kn shift_right 878787
+g512-led -kn arrow_top 878787
+g512-led -kn num1 878787
+g512-led -kn num2 878787
+g512-led -kn num3 878787
+g512-led -kn numenter 878787
+
+g512-led -kn ctrl_left 515151
+g512-led -kn win_left 515151
+g512-led -kn alt_left 515151
+g512-led -kn space 515151
+g512-led -kn alt_right 515151
+g512-led -kn win_right 515151
+g512-led -kn menu 515151
+g512-led -kn ctrl_right 515151
+g512-led -kn arrow_left 515151
+g512-led -kn arrow_bottom 515151
+g512-led -kn arrow_right 515151
+g512-led -kn num0 515151
+g512-led -kn num. 515151
+
+g512-led -c
