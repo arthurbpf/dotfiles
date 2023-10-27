4dd35150b8d0d6ef94579d1906433caf2a2fa8cf
Arthur Fernandes <arthurbpfernandes@gmail.com>
2023-10-26 21:14:55 -0300

    update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -106,14 +106,15 @@ require('lazy').setup({
   },
 
   {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     opts = {
       char = '┊',
       show_trailing_blankline_indent = false,
     },
+    config = function()
+      require("neorg").setup {}
+    end,
   },
 
   -- "gc" to comment visual regions/lines
