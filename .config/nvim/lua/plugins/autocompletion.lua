7a3129978c80a3c63475e0803045247e75c67f5d
Arthur Fernandes <arthurbpfernandes@gmail.com>
2023-12-13 17:35:18 -0300

    neovim config

--- .config/nvim/lua/plugins/autocompletion.lua
+++ .config/nvim/lua/plugins/autocompletion.lua
@@ -3,7 +3,13 @@ return {
   'hrsh7th/nvim-cmp',
   dependencies = {
     -- Snippet Engine & its associated nvim-cmp source
-    'L3MON4D3/LuaSnip',
+    {
+      "L3MON4D3/LuaSnip",
+      -- follow latest release.
+      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      -- install jsregexp (optional!).
+      build = "make install_jsregexp"
+    },
     'saadparwaiz1/cmp_luasnip',
 
     -- Adds LSP completion capabilities
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -8,4 +8,8 @@ return {
 
   --
   'numToStr/Comment.nvim',
+  {
+    'lervag/vimtex',
+
+  },
 }
